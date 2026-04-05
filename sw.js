935af998588e43e8b3cccffbf2e83d9df85dc557
anasfityan <73721048+anasfityan@users.noreply.github.com>
2026-04-05 05:17:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='speakpro-v10';
+const CACHE='speakpro-v11';
 const ASSETS=['./', './index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
